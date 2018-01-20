6338d9473e168ea5591bda5959eafe1b933221ee
Pierre Cormier <pierrecormier85@gmail.com>
2018-01-20 18:42:54 +0100

    comit

--- src/app/prono-list/prono-list.component.ts
+++ src/app/prono-list/prono-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import { Pronostic } from '../pronostic/pronostic';
 
 @Component({
   selector: 'app-prono-list',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prono-list.component.css']
 })
 export class PronoListComponent implements OnInit {
+  pronostics: Pronostic[];
+  matchday;
+  url = 'https://pronorest.herokuapp.com/api/';
+  // url = 'http://localhost:8080/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(this.url.concat('fixtures/current/'))
+    .toPromise().then(m => {
+        this.matchday = Number(m);
+      }
+    ).then(r => {
+      this.http.get<Pronostic[]>(this.url.concat('pronostic/').concat(this.matchday))
+      .toPromise().then(data => {
+          // Read the result field from the JSON response.
+          this.pronostics = data;
+        }
+      );
+    }
+    );
   }
 
 }
---
+++ src/app/pronostic/pronostic.ts
@@ -0,0 +1,13 @@
+export class Pronostic {
+    m1: string;
+    m2: string;
+    m3: string;
+    m4: string;
+    m5: string;
+    m6: string;
+    m7: string;
+    m8: string;
+    m9: string;
+    m10: string;
+    pseudo: string;
+  }
